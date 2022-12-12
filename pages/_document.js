cec779bba06eff0ef067fa3d805336dd12ca82d1
ayakh99 <ayaalaamohamed99@gmail.com>
2022-12-12 14:34:35 +0200

    fixed validation error

--- pages/_document.js
+++ pages/_document.js
@@ -2,10 +2,10 @@ import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html>
+    <Html lang="en-US">
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link
           href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@400;500;700&display=swap"
           rel="stylesheet"
